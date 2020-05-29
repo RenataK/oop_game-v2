f1b2fc5070b1f02411dcad0c532bf800d35bf5b6
Renata <rselleshiy@gmail.com>
2020-05-29 14:43:47 -0700

    Added additional code
    Added additional code to addPhraseToDisplay() Method

--- js/Phrase.js
+++ js/Phrase.js
@@ -11,11 +11,21 @@ class Phrase {
 * Display phrase on game board
 */
 addPhraseToDisplay() {
-    const ul = document.getElementById('phrase ul');
+    const ul = document.querySelector('#phrase ul');
     let li = document.createElement('LI');
     ul.appendChild(li);
-        
-};
 
+    for (let i=0; i<ul.length; i++) {
+        
+        let letter = this.phrase.split('');
+        li.innerHTML = letter;
+
+        if (this.phrase == ' ') {
+            li.classList = 'space';
+        } else if (this.phrase == letter) {
+            li.classList = 'hide letter';
+        }
+       }
+   }
 }
 
